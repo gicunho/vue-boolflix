e850fdb37dad12d91189bac1632025775f09bf2b
gicunho <gicunho@hotmail.it>
2021-06-08 17:55:26 +0200

    Add axios for series

--- Milestone 2/assets/js/main.js
+++ Milestone 2/assets/js/main.js
@@ -1,17 +1,22 @@
 const app = new Vue({
     el: '#root',
     data: {
-        url: '',
+        moviesUrl: '',
+        seriesUrl: '',
         movies: null,
+        series: null,
         ricerca: '',
-        query: '',
     },
     methods: {
          cerca() {
-             this.url = `https://api.themoviedb.org/3/search/movie?api_key=2ad14df71d61543a5c0cbb44fb587a2c&query=${this.ricerca}`
-             axios.get(this.url).then(resp =>{
+             this.moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=2ad14df71d61543a5c0cbb44fb587a2c&query=${this.ricerca}`
+             axios.get(this.moviesUrl).then(resp =>{
                 console.log(resp.data.results);
                 this.movies = resp.data.results
+            }),
+            this.seriesUrl = `https://api.themoviedb.org/3/search/tv?api_key=2ad14df71d61543a5c0cbb44fb587a2c&query=${this.ricerca}`
+            axios.get(this.seriesUrl).then(response =>{
+                this.series = response.data.results
             })
          }
     },
